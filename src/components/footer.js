4504c86082357af0f929aaa5e891ea240ce5c229
Neo <neolwc@gmail.com>
2016-09-24 14:31:19 +0000

    Add footer component

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <div className="center">Made by <a href="http://wenchen.li/">Neo</a> for <a href="http://www.jam3.com/">Jam3</a> - 2016</div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Menu from './components/menu';
+import Footer from './components/footer';
 
 class Root extends React.Component {
   render() {
     return (
-      <header>
-        <Menu list={['text', 'twitter', 'carousel', 'share']} />
-      </header>
+      <div id="root">
+        <header>
+          <Menu list={['text', 'twitter', 'carousel', 'share']} />
+        </header>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     );
   }
 }
